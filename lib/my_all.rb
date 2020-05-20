d182a16e979570b32913184f8de7ab56072c287b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 21:52:13 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_all?(collection)
 i = 0
-block_return_values = []
+block_return_values = [] #declare new array
 while i < collection.length
   block_return_values << yield(collection[i]) #yield line. when code is run, it is going to send whatever is passed in as the argument to the block.
+#new values shoveled into new array
   i = i + 1
 end
 
@@ -17,4 +18,4 @@ end
 
 my_all?([1, 2, 3]) {|i| i < 2} #method called
 #when it hits yield(1), it is going to send 1 to the block and evaluate it, and send the return value back to yield.
-#my_all?([1, 2, 3]) {|1| 1 < 2} => true 
+#my_all?([1, 2, 3]) {|1| 1 < 2} => true
